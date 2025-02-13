9f7a8b9a0f2d4299527304de1710edde865432c2
mhakanterzi <trz.hakanterzi@gmail.com>
2025-02-13 19:43:05 +0300

    added new functions in double linked list

--- Data-Structers/Structures/DoubleLinkedList.cs
+++ Data-Structers/Structures/DoubleLinkedList.cs
@@ -31,7 +31,7 @@ public class DoubleLinkedList
     }
 
     // insert nwe node at the begining
-    public void InsertBegin(int data)
+    public Node InsertBegin(Node head,int data)
     {
         Node newNode = new Node(data); //create new node
 
@@ -41,24 +41,60 @@ public class DoubleLinkedList
         {
             head.prev = newNode; //new node connect to old head
         }
-        head = newNode; //set head a new node
+        return head = newNode; //set head a new node
+    }
+
+    public Node DeleteHead(Node head)
+    {
+        //change head
+        head = head.next;
+
+        if(head != null)
+        {
+            //deleting head preview. İts old head
+            head.prev = null;
+        }
+        return head;
     }
 
     // Same as insert begin but at the end
-    public void InsertEnd(int data)
+    public Node InsertEnd(Node head,int data)
     {
         Node newNode = new Node(data);
-        newNode.prev = tail;
 
-        if(tail != null)
+
+        if(head != null)
         {
-            tail.next = newNode;
+            Node curr = head;
+            while (curr.next != null)
+            {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+            newNode.prev = curr;
         }
-        tail = newNode;
+        return head;
+    }
+
+    public Node DeleteTail(Node head)
+    {
+        Node curr = head;
+        while (curr.next != null)
+        {
+            curr=curr.next;
+        }
+        if(curr.prev != null)
+        {
+            //deleting in list number. old tail
+            curr.prev.next = null;
+        }
+        //delete to curr
+        curr = null;
+        return head;
     }
 
 
-
+    //This for listing linked list
     public void PrintList(Node head)
     {
         Node curr = head;
@@ -72,6 +108,7 @@ public class DoubleLinkedList
 
     public void StartLinkedList()
     {
+        //Creating linked list
         Node head = new Node(1);
         head.next = new Node(2);
         head.next.prev = head;
@@ -79,6 +116,21 @@ public class DoubleLinkedList
         head.next.next.prev = head.next;
 
         Console.WriteLine("List: ");
+        PrintList(head);
+
+        Console.WriteLine("Insert to 0 First column (new head)");
+        head = InsertBegin(head,0);
+        PrintList(head);
+        Console.WriteLine("Insert to 4 Last column (new tail)") ;
+        head = InsertEnd(head,4);
+        PrintList(head);
+
+        Console.WriteLine("Deleting First column. (Deleting head. new head is next to head)");
+        head = DeleteHead(head);
+        PrintList(head);
+
+        Console.WriteLine("Deleting Last column. (Deleting tail. new tail is preview to tail)");
+        head = DeleteTail(head);
         PrintList(head);
     }
 }
